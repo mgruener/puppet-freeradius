93fe68bc88ed0c68877a19a0dac6bb4df2235dc5
Michael Gruener <michael.gruener@chaosmoon.net>
2014-08-23 13:20:14 +0200

    Change radius service name to the correct one

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,2 +1,30 @@
 class freeradius::params {
+  if $::kernel != 'Linux' {
+    fail("${module_name} is only supported on Linux")
+  }
+
+  # no debian support at the moment...
+  case $::operatingsystem {
+    /^Fedora/: {
+        $package = 'freeradius'
+        $service = 'radiusd.service'
+        $service_provider = 'systemd'
+      }
+    /^RedHat|^CentOS/: {
+        case $::operatingsystemmajrelease {
+          '5','6': {
+              $package = 'freeradius'
+              $service = 'radiusd'
+              $service_provider = 'redhat'
+            }
+          '7': {
+              $package = 'freeradius'
+              $service = 'radiusd.service'
+              $service_provider = 'systemd'
+            }
+          default: { fail("${module_name} is not supported on ${::operatingsystem} ${::opertingsystemmajrelease}") }
+        }
+      }
+    default: { fail("${module_name} is not supported on ${::operatingsystem}") }
+  }
 }
